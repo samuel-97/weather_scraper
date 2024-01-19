8c2b52aca719841c2a1799eed080140e3f4bf7c5
samuel-97 <149805515+samuel-97@users.noreply.github.com>
2024-01-19 11:28:02 -0500

    Update geocoder_requests.py
    removing OpenWeather API key

--- weather_scraper/Geo_Requests/geocoder_requests.py
+++ weather_scraper/Geo_Requests/geocoder_requests.py
@@ -2,7 +2,7 @@ import requests
 
 BASE_CALL_geocoder = "http://api.openweathermap.org/geo/1.0/direct?q="
 LOCATION = ['Bakersfield,CA,USA', 'New York City,NY,USA', 'Amissville,VA,USA', 'Medellin,ANT,CO']
-API_KEY = "e7e12faba846cdc8ec5dedaa05d09698"
+API_KEY = "e7e12faba846cdc8ec5dedaa05d09698" # API_KEY invalidated, get your own
 
 request_bin = {}
 
